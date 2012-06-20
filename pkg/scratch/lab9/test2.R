6c2cba02a73827b8cdb68c0611b4e5a2c1439c87
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-06-20 03:15:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/scratch/lab9/test2.R
+++ pkg/scratch/lab9/test2.R
@@ -2,6 +2,6 @@ rm(list=ls())
 source("pphyproevo.R")
 
 ## test run from 1:10
-system.time(res <- MLE.s(c(beta[1],gamma[1]),1:106))
+system.time(res <- MLE.s(c(beta[1],gamma[1]),noNAind))
 save(res,file="test2")
 
