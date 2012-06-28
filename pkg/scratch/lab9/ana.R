6bc9855e7d094c8947db56cfffeff7503470adde
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-06-28 04:36:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/scratch/lab9/ana.R
+++ pkg/scratch/lab9/ana.R
@@ -1,24 +1,5 @@
-##Look at results from optimizaiton done separately for 106 genes, where each of them has their own values for beta and gamma
-gene.dir <- "~/proteinevoutk20/pkg/Result/AllGeneRes/"
-
-
-get.mle <- function(num){
-  load(paste("gene",num,".RData",sep=""))
-  #keep(mle,num,sure=T)
-  save(mle,file=paste("gene",num,sep=""))
-}
-lapply(1:106,get.mle)
-
-##Extract the information of MLE of parameters for each gene
-get.par <- function(num){
-  load(paste("gene",num,sep=""))
-  return(mle$par$par)
-}
-##Do that for all genes and put them to an array
-sapply(1:106,get.par)
-
 get.val <- function(x,y){
-  file <- paste("bg.",x,".",y,sep="")
+  file <- paste("~/proteinevoutk20/pkd/scratch/newton/bg.",x,".",y,sep="")
   load(file)
 ##  par <- sapply(1:106, function(x) res[[x]]$par)
   val <- sapply(1:106, function(x) res[[x]]$objective)
