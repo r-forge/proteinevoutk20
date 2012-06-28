7026826e167c54139ae8b3304179886beb992f35
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-28 04:43:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/scratch/lab9/ana.R
+++ pkg/scratch/lab9/ana.R
@@ -1,5 +1,5 @@
 get.val <- function(x,y){
-  file <- paste("~/proteinevoutk20/pkd/scratch/newton/bg.",x,".",y,sep="")
+  file <- paste("~/proteinevoutk20/pkg/scratch/newton/bg.",x,".",y,sep="")
   load(file)
 ##  par <- sapply(1:106, function(x) res[[x]]$par)
   val <- sapply(1:106, function(x) res[[x]]$objective)
