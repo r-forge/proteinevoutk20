15e29515e9d42b69f63fd5e299a51e729a0d3925
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-20 18:51:46 +0000

    optimization for genes with s < 1 optim_slt1.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/scratch/lab9/optim_slt1.R
@@ -0,0 +1,5 @@
+source("~/proteinevoutk20/pkg/R/pphyproevo.R")
+load("~/proteinevoutk20/pkg/scratch/newton/sGrids.RData")
+system.time(mle <- MLE.bg(c(0.1,0.01),c(0,0),c(1,1),s.lt1.index,trace=1,multicore=TRUE))
+
+save(mle,file="mle.slt1.RData")
