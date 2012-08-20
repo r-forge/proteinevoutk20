e9785204a074225a6c55ed51f087b318bc55c776
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-08-20 19:17:03 +0000

    update findmle.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/findmle.R
+++ pkg/R/findmle.R
@@ -1,4 +1,4 @@
 load("~/proteinevoutk20/pkg/RData/sim2simple_1_100_0.1.RData")
 source("~/proteinevoutk20/pkg/R/pphyproevo.R")
-system.time(mle <- MLE_GTR_sw(c(0.1,0.01,0.001),rep(0,3),rep(1,3),trees[[1]],sim[1:4,],m=20,al,mumat,protein_op=op_seq,root=start_seq,trace=1))
+system.time(mle <- MLE_sw(c(0.1,0.01,0.001),rep(0,3),rep(1,3),trees[[1]],sim[1:4,],m=20,al,mumat,protein_op=op_seq,root=start_seq,trace=1))
 save.image(file="sim2simpleMle_1_100_0.1.RData",compress=TRUE)
