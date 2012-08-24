4595ffe1580b7705a37afdd9eab303d6d4b643c6
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-08-24 01:16:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hessian.R
+++ pkg/R/hessian.R
@@ -1,6 +1,4 @@
-source("~/proteinevoutk20/pkg/R/pphyproevo.R")
 source("~/proteinevoutk20/pkg/R/simulation.R")
-library(numDeriv)
 
 find_hessian <- function(mle,tree,data,alpha,MuMat,m=20,protein_op=NULL,root=NULL,bf=NULL,C=2,Phi=0.5,q=4e-7,Ne=1.37e07){
   negloglike <- function(para){
