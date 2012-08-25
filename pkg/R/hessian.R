026ae5ecce194ea2e17d71633ed64dbc2a43ba61
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-25 03:08:59 +0000

    update hessian with log parameter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hessian.R
+++ pkg/R/hessian.R
@@ -9,3 +9,14 @@ find_hessian <- function(mle,tree,data,alpha,MuMat,m=20,protein_op=NULL,root=NUL
 }
   return(hessian(negloglike,mle,method="Richardson"))
 }
+
+find_hessian_log<- function(mle,tree,data,alpha,MuMat,m=20,protein_op=NULL,root=NULL,bf=NULL,C=2,Phi=0.5,q=4e-7,Ne=1.37e07){
+  negloglike <- function(para){
+    print(para)
+    s = exp(para[1])
+    beta = exp(para[2])
+    gamma = exp(para[3])
+    return(ll_indep(s,alpha,beta,gamma,MuMat,tree,data,m,protein_op,root,bf,C,Phi,q,Ne))
+}
+  return(hessian(negloglike,mle,method="Richardson"))
+}
