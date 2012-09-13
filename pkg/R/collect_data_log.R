7ea7c3541b066cbe7320acfe178f051d9c5db006
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-13 07:01:30 +0000

    update R scripts
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/collect_data_log.R
+++ pkg/R/collect_data_log.R
@@ -4,16 +4,32 @@ numsites <- c(100,300,500,700,1000)
 svalues <- c(0.1,0.3,0.5,0.7,0.9,1)
 ##tips of trees used in simulation
 tip <- c(4,4,8,8,12,12,10,10,14,14,16,16,18,18)
-#tip <- c(4,4,8,8,12,12)
-#tip <- c(10,10,14,14,16,16,18,18)
 ##result to store all the data
-datares14 <- array(list(NULL),dim=c(14,5,6))
+datares <- array(list(NULL),dim=c(14,5,6))
 ##change this to "simMle" for information on MLE without log
-fileprefix <- "simMleLog_"
-DataFile <- "collect_data_log.RData"
-##fileprefix <- "simMle"
-##DataFile <- "collect_data.RData"
+fileprefix <- "op_simu_"
+DataFile <- "op1_short.RData"
+
 for(treeindex in 1:14){
+  for(sitesindex in 1:5){
+    for(sindex in 1:6){
+      print(paste("tree ",treeindex,", ",numsites[sitesindex], " sites, s = ",svalues[sindex], sep=""))
+      RDataName = paste(fileprefix,treeindex,"_",numsites[sitesindex],"_",svalues[sindex],".RData",sep="")
+      if(file.exists(RDataName)){
+        load(RDataName)
+        datares[[treeindex,sitesindex,sindex]]$para <- log(c(svalues[sindex],be,ga))
+        datares[[treeindex,sitesindex,sindex]]$mle_op <- mle_log_op$par
+        datares[[treeindex,sitesindex,sindex]]$mle_nop <- mle_log_nop$par
+        datares[[treeindex,sitesindex,sindex]]$hes_op <- hes_op
+        datares[[treeindex,sitesindex,sindex]]$hes_nop <- hes_nop
+        }
+      else
+        print("file doesn't exist!")
+    }
+  }
+}
+fileprefix <- "op_simu_"
+for(treeindex in 15:18){
   for(sitesindex in 1:5){
     for(sindex in 1:6){
       print(paste("tree ",treeindex,", ",numsites[sitesindex], " sites, s = ",svalues[sindex], sep=""))
@@ -21,27 +37,16 @@ for(treeindex in 1:14){
       if(file.exists(RDataName)){
         print("file exists!")
         load(RDataName)
-        datares14[[treeindex,sitesindex,sindex]]$para <- log(c(svalues[sindex],be,ga))
-        datares14[[treeindex,sitesindex,sindex]]$mle <- mle_log$par
-        rm(mle)
-        if(exists("hes")){
-          datares14[[treeindex,sitesindex,sindex]]$hessian <- hes
-          datares14[[treeindex,sitesindex,sindex]]$eigenval <- eigen(hes)$values
-          datares14[[treeindex,sitesindex,sindex]]$invhes <- solve(hes)
-          print("remove hessian now")
-          rm(hes)
-        }
-        else{
-          print("hessian not calculated")
-##           datares14[[treeindex,sitesindex,sindex]]$hessian <- NULL
-##           datares14[[treeindex,sitesindex,sindex]]$eigenval <- NULL
-##           datares14[[treeindex,sitesindex,sindex]]$invhes <- NULL
-        }
+        datares[[treeindex,sitesindex,sindex]]$para <- log(c(svalues[sindex],be,ga))
+        datares[[treeindex,sitesindex,sindex]]$mle_op <- mle_log_op$par
+        datares[[treeindex,sitesindex,sindex]]$mle_nop <- mle_log_nop$par
+        datares[[treeindex,sitesindex,sindex]]$hes_op <- hes_op
+        datares[[treeindex,sitesindex,sindex]]$hes_nop <- hes_nop
       }
       else
         print("file doesn't exist!")
     }
   }
 }
-save(datares14,file=DataFile)
+save(datares,file=DataFile)
 
--- pkg/R/plot_sim.R
+++ pkg/R/plot_sim.R
@@ -24,7 +24,7 @@ vec.mle <- Vectorize(get.mle,vectorize.args=c("treeind","siteind"))
 ## paraind specifies the index of parameters(1:s,2:beta,3:gamma)
 plot.s <- function(sind,paraind,op=T){
   ## find mle for all the simulations, stored in an array (6*5)
-  d <- outer(1:18,1:5,vec.mle,sind=sind,paraind=paraind,op=op)
+  d <- outer(1:14,1:5,vec.mle,sind=sind,paraind=paraind,op=op)
   ## parameter s                                                                                                     
   if(paraind==1){
     d <- abs(d-log(svalues[sind]))
@@ -59,20 +59,22 @@ plot.s <- function(sind,paraind,op=T){
   points(numsites,d[12,],type="l",col="tan")
   points(numsites,d[13,],type="l",col="tan")
   points(numsites,d[14,],type="l",col="tan")
-  points(numsites,d[15,],type="l",col="darkviolet")
-  points(numsites,d[16,],type="l",col="darkviolet")
-  points(numsites,d[17,],type="l",col="seagreen")
-  points(numsites,d[18,],type="l",col="seagreen")
-  leg.txt <- c("4 tips","8 tips","10 tips","12 tips","14 tips","16 tips","18tips")
+#   points(numsites,d[15,],type="l",col="darkviolet")
+#   points(numsites,d[16,],type="l",col="darkviolet")
+#   points(numsites,d[17,],type="l",col="seagreen")
+#   points(numsites,d[18,],type="l",col="seagreen")
+  #leg.txt <- c("4 tips","8 tips","10 tips","12 tips","14 tips","16 tips","18tips")
+  leg.txt <- c("4 tips","8 tips","10 tips","12 tips","14 tips")
   #y.leg <- seq(from=max(d)+max(d)*0.1,by=-max(d)*0.1,length.out=7)
-  col.leg <- c("black","blue","red","yellow","tan","darkviolet","seagreen")
+  #col.leg <- c("black","blue","red","yellow","tan","darkviolet","seagreen")
+  col.leg <- c("black","blue","red","yellow","tan")
   legend("topright",leg.txt,col=col.leg,text.col=col.leg,
            lty=1,bty="n",y.intersp=0.5,cex=0.5)
 }
   
 plot.s.mean <- function(sind,paraind,op=T){
   ## find mle for all the simulations, stored in an array (6*5)
-  d <- outer(1:18,1:5,vec.mle,sind=sind,paraind=paraind,op=op)
+  d <- outer(1:14,1:5,vec.mle,sind=sind,paraind=paraind,op=op)
   ## parameter s                                                                                                     
   if(paraind==1){
     d <- abs(d-log(svalues[sind]))
@@ -92,17 +94,19 @@ plot.s.mean <- function(sind,paraind,op=T){
     plot.ylab = "absolute error of log(gamma)"
   }
   
-  plot(numsites,d[1,],type="l",xlab="number of sites",ylab=plot.ylab,ylim=c(min(d),min(max(d)+max(d)*0.1,2.5)))
+  plot(numsites,d[1,],type="l",xlab="number of sites",ylab=plot.ylab,ylim=c(min(d),max(d)+max(d)*0.1))
   title(plot.title)
   points(numsites,apply(d[3:4,],2,mean),type="l",col="blue")
   points(numsites,apply(d[5:6,],2,mean),type="l",col="red")
   points(numsites,apply(d[7:8,],2,mean),type="l",col="yellow")
-  points(numsites,apply(d[9:14,],2,mean),type="l",col="tan")
-  points(numsites,apply(d[15:16,],2,mean),type="l",col="darkviolet")
-  points(numsites,apply(d[17:18,],2,mean),type="l",col="seagreen")
+  points(numsites,apply(d[9:10,],2,mean),type="l",col="tan")
+  points(numsites,apply(d[11:12,],2,mean),type="l",col="darkviolet")
+  points(numsites,apply(d[13:14,],2,mean),type="l",col="seagreen")
   leg.txt <- c("4 tips","8 tips","10 tips","12 tips","14 tips","16 tips","18tips")
-  #y.leg <- seq(from=max(d)+max(d)*0.1,by=-max(d)*0.1,length.out=7)
+  #leg.txt <- c("4 tips","8 tips","10 tips","12 tips","14 tips")
+
   col.leg <- c("black","blue","red","yellow","tan","darkviolet","seagreen")
+  #col.leg <- c("black","blue","red","yellow","tan")
   legend("topright",leg.txt,col=col.leg,text.col=col.leg,
          lty=1,bty="n",y.intersp=0.5,cex=0.5)
 }
