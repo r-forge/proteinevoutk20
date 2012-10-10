f319e337edd9fca10fce3ac293e722798528ce46
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-10-10 17:18:28 +0000

    update the R file that collects results from simulation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/collect_data_log.R
+++ pkg/R/collect_data_log.R
@@ -1,50 +1,32 @@
 ##numer of sites in the simulation
-numsites <- c(100,300,500,700,1000)
+numsites <- c(500,800,1000,2000,3000)
 ##s values used in simulation
-svalues <- c(0.1,0.3,0.5,0.7,0.9,1)
+svals <- c(0.1,0.3,0.5,0.7,0.9)
 ##tips of trees used in simulation
-tip <- c(4,4,8,8,12,12,10,10,14,14,16,16,18,18)
+tip <- c(2,4,8,16)
+## edge length scale factor
+esvalues <- c(1,10)
 ##result to store all the data
-datares <- array(list(NULL),dim=c(14,5,6))
+datares <- array(list(NULL),dim=c(4,5,5,2))
 ##change this to "simMle" for information on MLE without log
-fileprefix <- "op_simu_"
-DataFile <- "op1_short.RData"
+fileprefix <- "sim_"
+DataFile <- "summary.RData"
 
-for(treeindex in 1:14){
+for(treeindex in 1:4){
   for(sitesindex in 1:5){
-    for(sindex in 1:6){
-      print(paste("tree ",treeindex,", ",numsites[sitesindex], " sites, s = ",svalues[sindex], sep=""))
-      RDataName = paste(fileprefix,treeindex,"_",numsites[sitesindex],"_",svalues[sindex],".RData",sep="")
-      if(file.exists(RDataName)){
-        load(RDataName)
-        datares[[treeindex,sitesindex,sindex]]$para <- log(c(svalues[sindex],be,ga))
-        datares[[treeindex,sitesindex,sindex]]$mle_op <- mle_log_op$par
-        datares[[treeindex,sitesindex,sindex]]$mle_nop <- mle_log_nop$par
-        datares[[treeindex,sitesindex,sindex]]$hes_op <- hes_op
-        datares[[treeindex,sitesindex,sindex]]$hes_nop <- hes_nop
-        }
-      else
-        print("file doesn't exist!")
-    }
-  }
-}
-fileprefix <- "op_simu_"
-for(treeindex in 15:18){
-  for(sitesindex in 1:5){
-    for(sindex in 1:6){
-      print(paste("tree ",treeindex,", ",numsites[sitesindex], " sites, s = ",svalues[sindex], sep=""))
-      RDataName = paste(fileprefix,treeindex,"_",numsites[sitesindex],"_",svalues[sindex],".RData",sep="")
-      if(file.exists(RDataName)){
-        print("file exists!")
-        load(RDataName)
-        datares[[treeindex,sitesindex,sindex]]$para <- log(c(svalues[sindex],be,ga))
-        datares[[treeindex,sitesindex,sindex]]$mle_op <- mle_log_op$par
-        datares[[treeindex,sitesindex,sindex]]$mle_nop <- mle_log_nop$par
-        datares[[treeindex,sitesindex,sindex]]$hes_op <- hes_op
-        datares[[treeindex,sitesindex,sindex]]$hes_nop <- hes_nop
-      }
-      else
-        print("file doesn't exist!")
+    for(sindex in 1:5){
+      for(esind in 1:2){
+        cat("tree", treeindex, ",", numsites[sitesindex], "sites, s=",svals[sindex],"edge length *",esvalues[esind], "\n")
+        RDataName = paste(fileprefix,treeindex,"_",numsites[sitesindex],"_",svals[sindex],"_",esvalues[esind],".RData",sep="")
+        if(file.exists(RDataName)){
+          load(RDataName)
+          datares[[treeindex,sitesindex,sindex,esind]]$op <- sw_op$par
+          datares[[treeindex,sitesindex,sindex,esind]]$est <- sw_est$par
+          datares[[treeindex,sitesindex,sindex,esind]]$mode <- sw_mode$par
+         }
+       else
+         cat("file doesn't exist!","\n")
+       }
     }
   }
 }
