4b50b7fe357ff132d6eea3febef6156aaabea6a0
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-26 17:09:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@229 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/rokas_collect.R
@@ -0,0 +1,13 @@
+setwd("~/BackupProEvo/rokas_max/")
+res_max <- vector("list",length=106)
+for(genect in 1:l){
+  filename = paste("gene",genect,"_s_weight.RData",sep="")
+  cat("load RData for gene", genect,"\n")
+  load(filename)
+  res_max[[genect]] <- res_op
+}
+
+s_max <- sapply(1:106,function(x) res_max[[x]]$s)
+loglik_max <- sapply(1:106,function(x) res_max[[x]]$ll$loglik)
+GM_max <- sapply(1:106,function(x) res_max[[x]]$GMweights)
+Q_max <- sapply(1:106,function(x) res_max[[x]]$Q)
