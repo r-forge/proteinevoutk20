8684629d11ba41537233e45443fb898d8cd0918a
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-26 03:46:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@228 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hessian.R
+++ pkg/R/hessian.R
@@ -1,22 +1,12 @@
-source("~/proteinevoutk20/pkg/R/simulation.R")
-
-find_hessian <- function(mle,tree,data,alpha,MuMat,m=20,protein_op=NULL,root=NULL,bf=NULL,C=2,Phi=0.5,q=4e-7,Ne=1.37e07){
-  negloglike <- function(para){
+#source("~/proteinevoutk20/pkg/R/simulation.R")
+# find the hessian matrix at the mle for c(s,beta,gamma) for the -loglikelihood function
+find_hessian <- function(mle,data,tree,...){
+  fn <- function(para){
+    print(para)
     s = para[1]
     beta = para[2]
     gamma = para[3]
-    return(ll_indep(s,alpha,beta,gamma,MuMat,tree,data,m,protein_op,root,bf,C,Phi,q,Ne))
-}
-  return(hessian(negloglike,mle,method="Richardson"))
-}
-
-find_hessian_log<- function(mle,tree,data,alpha,MuMat,m=20,protein_op=NULL,root=NULL,bf=NULL,C=2,Phi=0.5,q=4e-7,Ne=1.37e07){
-  negloglike <- function(para){
-    print(para)
-    s = exp(para[1])
-    beta = exp(para[2])
-    gamma = exp(para[3])
-    return(ll_indep(s,alpha,beta,gamma,MuMat,tree,data,m,protein_op,root,bf,C,Phi,q,Ne))
-}
-  return(hessian(negloglike,mle,method="Richardson"))
+    return(-mllm(data,tree,s=s,beta=beta,gamma=gamma,...)$ll$loglik)
+  }
+  return(hessian(fn,mle,method="Richardson"))
 }
