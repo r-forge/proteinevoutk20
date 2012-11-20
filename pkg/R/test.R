546c96fe2f5b6c115974b0f105f60e6de405f6bd
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-11-20 20:28:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@255 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test.R
+++ pkg/R/test.R
@@ -10,6 +10,7 @@ optim.opw.subplex <- function(data, tree,opw=rep(1/20,20), ...){
   fn = function(lopw,data,tree, ...){
     opw = exp(c(lopw,0))
     opw=opw/sum(opw)
+    cat(opw,"\n")
     result = mllm(data=data,tree=tree,opw=opw, ...)$ll$loglik
     return(result)
   }
@@ -20,5 +21,24 @@ optim.opw.subplex <- function(data, tree,opw=rep(1/20,20), ...){
   opw = exp(c(res[[1]],0))
   opw = opw/sum(opw)
   res$par = opw
+  return(res)
+}
+
+#MLE for s, beta and gamma, using Nelder-Mead method by default
+#mllm <- function(data,tree,s,beta=be,gamma=ga,Q=NULL,
+#             dismat=NULL,mumat=NULL,opaa=NULL,opw=NULL,bfaa=NULL,C=2,Phi=0.5,q=4e-7,Ne=5e6)
+#sample call : optim.s.weight(gene1,ROKAS_TREE,0.1,be,ga,Q=NU_VEC)
+optim.s.weight.subplex <- function(data, tree, s,beta,gamma, ...){
+  ab <- c(s,beta,gamma)
+  ab[ab==0] <- 1e-08 #take care of log(0)
+  ab <- log(ab)
+  fn = function(ab,data,tree, ...){
+    ab = exp(ab)
+    cat(ab,"\n")
+    result = mllm(data=data,tree=tree,s=ab[1],beta=ab[2],gamma=ab[3], ...)$ll$loglik
+    return(result)
+  }
+  res = subplex(par=ab,fn=fn,data=data,tree=tree,...)
+  res$par = exp(res$par)
   return(res)
 }
