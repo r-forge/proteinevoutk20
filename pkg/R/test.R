5cc3f2b455e7c0bd9fe52fc024a0a73efe318c12
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-24 00:39:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@256 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test.R
+++ pkg/R/test.R
@@ -28,17 +28,30 @@ optim.opw.subplex <- function(data, tree,opw=rep(1/20,20), ...){
 #mllm <- function(data,tree,s,beta=be,gamma=ga,Q=NULL,
 #             dismat=NULL,mumat=NULL,opaa=NULL,opw=NULL,bfaa=NULL,C=2,Phi=0.5,q=4e-7,Ne=5e6)
 #sample call : optim.s.weight(gene1,ROKAS_TREE,0.1,be,ga,Q=NU_VEC)
-optim.s.weight.subplex <- function(data, tree, s,beta,gamma, ...){
+optim.s.weight.pso <- function(data, tree, s,beta,gamma, Q){
   ab <- c(s,beta,gamma)
-  ab[ab==0] <- 1e-08 #take care of log(0)
-  ab <- log(ab)
-  fn = function(ab,data,tree, ...){
-    ab = exp(ab)
-    cat(ab,"\n")
-    result = mllm(data=data,tree=tree,s=ab[1],beta=ab[2],gamma=ab[3], ...)$ll$loglik
+#   ab[ab==0] <- 1e-08 #take care of log(0)
+#   ab <- log(ab)
+  fn = function(ab){
+#     ab = exp(ab)
+    result = mllm(data=data,tree=tree,s=ab[1],beta=ab[2],gamma=ab[3], Q=Q)$ll$loglik
+    cat("par:",ab,"val:",result,"\n")
     return(result)
   }
-  res = subplex(par=ab,fn=fn,data=data,tree=tree,...)
+  res = optim_pso(objective_function=fn,number_of_parameters=3,number_of_particles=4,max_number_of_iterations=100,
+                  parameter_bounds=cbind(rep(0,3),rep(10,3)),initial_estimates=matrix(ab,ncol=1),logfile="ppso.log",load_projectfile="no",
+                  do_plot="base")
   res$par = exp(res$par)
   return(res)
+}
+
+f <- function(sd, Ne){
+
+  if(sd==0)
+    return(1/(2*Ne))
+  else{
+    sd = mpfr(sd,prec=10000)
+    Ne = mpfr(Ne,prec=10000)
+    return((1-exp(sd))/(1-exp(sd*2*Ne)))
+  }
 }
