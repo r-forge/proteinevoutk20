cff9a759ae76e55b90ea055335268dc5fe577ad4
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-10 17:18:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@278 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test.R
+++ pkg/R/test.R
@@ -1,28 +1,27 @@
-## MLE for opw (weights for optimal amino acids)
+## MLE for opw (weights for optimal amino acids), using simulated anealing method -- GenSA
 #mllm <- function(data,tree,s,beta=be,gamma=ga,Q=NULL,
 #             dismat=NULL,mumat=NULL,opaa=NULL,opw=NULL,bfaa=NULL,C=2,Phi=0.5,q=4e-7,Ne=5e6)
 #sample call: optim.opw.subplex(data,tree,s=0.1,Q=NU_VEC...)
-optim.opw.new <- function(data, tree,opw=rep(1/20,20),method="Nelder-Mead", maxit=500, trace=0, ...){
+optim.opw.GenSA <- function(data, tree,opw=NULL, ...){
+  if(is.null(opw))
+    opw = findBf2(data)
   l = length(opw)
   nenner = 1/opw[l]
   lopw = log(opw*nenner) #scale the vector by the last entry
   lopw = lopw[-l] # optimize on the all entries except the last one
-  res = mllm(data,tree,...)
-  llmat = res$ll$llmat
-  weight = attr(res$data,"weight")
   fn = function(lopw,data,tree, ...){
     opw = exp(c(lopw,0))
     opw=opw/sum(opw)
-    result = weight %*% llmat %*% opw
+    result = -mllm(data=data,tree=tree,opw=opw, ...)$ll$loglik
     cat("par:",opw,"val:",result,"\n")
     return(result)
   }
-  res = optim(par=lopw,fn=fn,gr=NULL,method=method,lower=-Inf,upper=Inf,
-              control=list(fnscale=-1,trace=trace,maxit=maxit),data=data,tree=tree, ...)
+  res = GenSA(par=lopw,fn=fn,lower=rep(-1000,19),upper=rep(100,19),
+              control=list(verbose=TRUE,maxit=100),data=data,tree=tree, ...)
   #print(res[[2]])
-  opw = exp(c(res[[1]],0))
+  opw = exp(c(res$par,0))
   opw = opw/sum(opw)
-  res$par = opw
+  res$para = opw
   return(res)
 }
 #MLE for s, beta and gamma, using Nelder-Mead method by default
