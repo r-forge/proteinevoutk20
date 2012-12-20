1752b689aa350391bc495bfca914984052d7c4e5
chaij (JJ Chai) <chaij@umail.iu.edu>
2012-12-20 20:13:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/scratch.R
+++ pkg/R/scratch.R
@@ -1,29 +1,89 @@
-optim.opw <- function(data, tree,opw=NULL, bad.value=-10000000, ...){
-  if(is.null(opw))
-    opw = findBf2(data)
-  opw = opw/sum(opw) #opw given in the function call doesn't have to sum to 1
-  
-  res = mllm(data=data,tree=tree,opw=opw,...) #store llmat (loglikelihood values for all opaa) 
-  llmat = exp(res$ll$llmat)    #so that they don't need to be evaluated again and again
-  weight = attr(data,"weight")
-  print(res$ll$loglik) #function value at the starting point
-  
-  # function to optimize on and its gradient function
-  eval_f_list <- function(opw){
-    return(list("objective"=llaaw1(opw,weight=weight,llmat=llmat),
-                "gradient"=llaaw_grad(opw,weight=weight,llmat=llmat)))
+optr.w <- function(beta,gamma,generange,tree,multicore=FALSE,...){
+  ## a function of x that return the sum of -loglikelihood values for all genes with s optimized separately for different genes
+  ab <- c(beta,gamma)
+  fn <- function(ab,generange,tree){
+    #print(ab)
+    mle <- optim.s.range(ab[1],ab[2],generange,tree=tree,multicore=multicore,...) #call the previous function to optimize s for all genes
+    mle.val <- sapply(1:length(generange),function(ind) mle[[ind]]$objective) # best -loglikelihood values
+    return(sum(mle.val)) #summation of all values
   }
-  # linear equality constraint, and its jacobian
-  eval_g_list <- function(opw){
-    return(list("constraints"=sum(opw)-1,"jacobian"=rep(1,20)))
-  }
-  lower <- rep(0,20) #lower bound
-  upper <- rep(1,20) #upper bound
-  local_opts <- list("algorithm"="NLOPT_LD_MMA","xtol_rel"=1e-7) #options for local optimizer
+  
+  lower <- c(0,0) #lower bound
+  upper <- c(Inf,Inf) #upper bound
   #options for optimizer
-  opts <- list("algorithm"="NLOPT_LD_AUGLAG","maxeval"="1000000","xtol_rel"=1e-7,"ftol_rel"=.Machine$double.eps,"local_opts"=local_opts)
-  res = nloptr(x0=opw,eval_f=eval_f_list,eval_g_eq=eval_g_list, lb=lower,ub=upper,opts=opts)
-  # res$objective: best function value found
-  # res$solution: best parameter values
+  opts <- list("algorithm"="NLOPT_LN_SBPLX","maxeval"="1000000","xtol_rel"=1e-7,"ftol_rel"=.Machine$double.eps^0.5,"print_level"=1)
+  res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,generange=generange,tree=tree)
+  return(res)
+}
+#sample call : optim.s.weight(gene1,ROKAS_TREE,0.1,be,ga,trace=1,Q=NU_VEC))
+optr.s.weight <- function(data, tree, s,beta,gamma, ...){
+  #store information from initial condition, with other parameters fixed
+  res.initial = mllm1(data=data,tree=tree,s=s,beta=beta,gamma=gamma,...)
+  #these don't change with the change of s
+  mumat = res.initial$mumat
+  bfaa=res.initial$bfaa
+  
+  ab <- c(s,beta,gamma) ##initial value
+  fn = function(ab,data,tree){
+    print(ab)
+    result = -mllm1(data=data,tree=tree,s=ab[1],beta=ab[2],gamma=ab[3], mumat=mumat,bfaa=bfaa, ...)$ll$loglik
+    return(result)
+  }
+  lower <- rep(0,3)
+  upper <- rep(Inf,3)
+  #options for optimizer
+  opts <- list("algorithm"="NLOPT_LN_COBYLA","maxeval"="1000000","xtol_rel"=1e-7,"ftol_rel"=.Machine$double.eps^0.5,"print_level"=1)
+  res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,data=data,tree=tree)
+  return(res)
+}
+
+optimQ <- function(tree,data,Q=rep(1,6), ...){
+  Q = Q/Q[6] #make last rate 1
+  #store information from initial condition, with other parameters fixed
+  res.initial = mllm1(data=data,tree=tree,Q=Q,...)
+  #these don't change with the change of s
+  fixmatall = res.initial$fixmatall
+  bfaa = res.initial$bfaa
+  
+  ab <- Q[1:5] # optimize on the first 5 rates
+  fn = function(ab,tree,data){
+    print(ab)
+    result = -mllm1(data,tree,Q=c(ab,1),fixmatall=fixmatall,bfaa=bfaa, ...)$ll$loglik
+    return(result)
+  }
+  lower=rep(0,5)
+  upper=rep(Inf,5)
+  #options for optimizer
+  opts <- list("algorithm"="NLOPT_LN_PRAXIS","maxeval"="1000000","xtol_rel"=1e-7,"ftol_rel"=.Machine$double.eps^0.5,"print_level"=1)
+  res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,data=data,tree=tree)
+  res$solution = c(res$solution,1) # append the last rate (1) to the rate vector
+  #print(res)
+  return(res)
+}
+
+optimBr <- function(data,tree,el=NULL,stopval=-Inf,method="NLOPT_LN_SBPLX", ...){
+  if(is.null(attr(tree,"order")) || attr(tree,"order") == "cladwise")
+    tree <- reorderPruning(tree)
+  if(is.null(el))
+    {el <- tree$edge.length}
+  br.num <- length(el)
+  
+  res.initial = mllm1(data=data,tree=tree,...)
+  #these don't change with the change of s
+  Qall = res.initial$Qall
+  bfaa = res.initial$bfaa
+  fn = function(el,data,tree){
+    tree$edge.length = el
+    print(el)
+    result = -mllm1(data,tree, Qall=Qall,bfaa=bfaa,...)$ll$loglik
+    return(result)
+  }
+  lower=rep(0,br.num)
+  upper=rep(Inf,br.num)
+  #options for optimizer
+  opts <- list("algorithm"=method,"maxeval"="1000000","xtol_rel"=1e-5,"ftol_rel"=.Machine$double.eps^0.5,
+               "stopval"=stopval,"print_level"=1)
+  res = nloptr(x0=el,eval_f=fn, lb=lower,ub=upper,opts=opts,data=data,tree=tree)
+  print(res)
   return(res)
 }
