537468edf386adc48d0d5d54e90a5c10ba10825e
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-16 21:18:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@336 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/scratch/lab9/mle_7.R
+++
@@ -1,8 +0,0 @@
-source("~/proteinevoutk20/pkg/R/main.R") 
- gene7 = conv("gene7.fasta","phyDat")
-tree = read.tree("gene7prune.tre")
- res=mllm(gene7,tree,0.1,be,ga,Q=NU_VEC) 
- res$ll$loglik 
- res_op = optim.mllm(res,optQ=T,optBranch=T,optsWeight=T,optOpw=FALSE,control=list(epsilon=1e-08,hmaxit=500,htrace=1,print_level=0,maxeval="50")) 
- res_op$ll$loglik 
- save.image(file="gene7.RData",compress=TRUE) 
