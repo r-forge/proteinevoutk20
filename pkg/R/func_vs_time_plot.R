70e460edfc0db4fd4d4d8782f6a567649f005741
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2013-01-23 21:25:13 +0000

    function for making the right hand plot in the grant
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@361 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/func_vs_time_plot.R
+++ pkg/R/func_vs_time_plot.R
@@ -2,7 +2,7 @@
 ##################################################################################################
 ## read in the MLE of parameters for all genes under max rule, s_max stores the g values
 ##################################################################################################
-maxdir <- "~/BackupProEvo/Newton/rokas_max/"
+maxdir <- "~/Dropbox/CollabChaiGilchristOMeara/GrantFigure2/rokas_max/"
 res_max <- vector("list",length=106)
 l <- 106
 for(genect in 1:l){
@@ -16,7 +16,7 @@ for(genect in 1:l){
 s_max <- sapply(1:106,function(x) res_max[[x]]$s)
 rm(list=setdiff(ls(),"s_max")) #only save the object s_max
 
-source("~/proteinevoutk20/pkg/R/main.R")
+source("~/Documents/MyDocuments/Active/proteinevoutk20/pkg/R/main.R")
 ##################################################################################################
 ## Plot functionality along the simulated evolution paths, starting from sequences
 ## drawn from both our model and WAG model
@@ -24,7 +24,7 @@ source("~/proteinevoutk20/pkg/R/main.R")
 ## random: should the simulations be drawn from the 100 randomly or the first several?
 ##################################################################################################
 plot.func <- function(gene_num,count=3,margin=0.1,random=TRUE){
-  filename <- paste("~/BackupProEvo/Lab9/Wagprunetree/wgene",gene_num,".RData",sep="") #RData file with simulated data
+  filename <- paste("~/Dropbox/CollabChaiGilchristOMeara/GrantFigure2/Wagprunetree/wgene",gene_num,".RData",sep="") #RData file with simulated data
   load(filename)
   obs.fty <- Ftny_protein(protein=datanum[6,],protein_op=opaa,s=s,DisMat=dismat) #functionality of observed sequence
   ## find the limits for y-axis
@@ -37,15 +37,21 @@ plot.func <- function(gene_num,count=3,margin=0.1,random=TRUE){
   else
     index <- 1:count
   ##start a plot
-  plot(sim[[index[1]]]$ftyfun,do.points=FALSE,xaxs="i",col="red",xlab="time",ylab="functionality",ylim=ylim,xlim=c(0,brlen),
-       main=paste("gene",gene_num,",  g*Phi=",round(s_max[gene_num],2),sep="")) 
-  abline(h=obs.fty,col="green",lwd=2) 
+  #plot(sim[[index[1]]]$ftyfun,do.points=FALSE,xaxs="i",col="red",xlab="time",ylab="functionality",ylim=c(0,1),xlim=c(0,brlen),
+  #     main=paste("gene",gene_num,",  g*Phi=",round(s_max[gene_num],2),sep=""), bty="n") 
+  plot(sim[[index[1]]]$ftyfun,do.points=FALSE,xaxs="i",col="red",xlab="Time",ylab="Functionality",ylim=c(0,1),xlim=c(0,brlen),
+       main="Functionality vs time", bty="n",xaxt="n") 
+  abline(h=obs.fty,col="black",lwd=4, lty="dotted")
   for(i in index){
-    plot(sim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
-    plot(simWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
-    plot(wsim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
-    plot(wsimWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
+    plot(sim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col=rgb(1,0,0,.4),add=TRUE)
+    plot(simWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col=rgb(0,0,1,.4),add=TRUE)
+    plot(wsim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col=rgb(1,0,0,.4),add=TRUE)
+    plot(wsimWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col=rgb(0,0,1,.4),add=TRUE)
   }
+ # axis(1, at=seq(from=0, to=1, length.out=5)*brlen, labels=round(seq(from=0, to=1, length.out=5),2))
+  axis(1, at=seq(from=0, to=1, length.out=5)*brlen, labels=round(seq(from=0, to=1, length.out=5)*brlen, 2))
+  #axis(1)
 }
 ##call of function: (gene72 has the highest g*Phi, gene83 has the lowest)
-## plot.func(1,count=10,margin=0.01,random=T)
+## plot.func(72,count=10,margin=0.01,random=T)
+plot.func(72,count=10,margin=0.01,random=T)
