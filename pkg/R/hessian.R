b72a4382d2a5d5cc604f918c18f8688b176d0c3f
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-26 04:17:46 +0000

    add code in scratch.R to find standard deviation at the MLE for g, beta and gamma
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@369 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hessian.R
+++ pkg/R/hessian.R
@@ -1,12 +1,15 @@
 #source("~/proteinevoutk20/pkg/R/simulation.R")
 # find the hessian matrix at the mle for c(s,beta,gamma) for the -loglikelihood function
+# since it's the -loglikelihood, the covariance matrix will just be the inverse of hessian
+# instead of the inverse of the -hessian
+# square roots of diagonals from the inverse hessian will give the standard errors
 find_hessian <- function(mle,data,tree,...){
   fn <- function(para){
-    print(para)
+    #print(para)
     s = para[1]
     beta = para[2]
     gamma = para[3]
-    return(-mllm(data,tree,s=s,beta=beta,gamma=gamma,...)$ll$loglik)
+    return(-mllm1(data,tree,s=s,beta=beta,gamma=gamma,...)$ll$loglik)
   }
   return(hessian(fn,mle,method="Richardson"))
 }
