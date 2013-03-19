3c32c215d549bba1e2ba06ca2fc4a94353a0eb44
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-19 14:11:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@392 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Identifiability.R
+++ pkg/R/Identifiability.R
@@ -1,6 +1,6 @@
 
 
-phi.sd <- 0.05
+phi.sd <- 0.5
 phig.sd <- 0.01
 
 
@@ -20,22 +20,33 @@ LikelihoodForOptim <- function(x, logphi.vector, logphig.vector) {
   return(GetLikelihoodMany(logphi.vector=logphi.vector, logphig.vector=logphig.vector, a=x[1], b=x[2], c=exp(x[3]))) 
 }
 
-nreps<-2
-ngenes<-1060
+
+
+
+nreps<-20
+ngenes<-106
 true.vals<-matrix(ncol=3, nrow=0)
 est.vals<-matrix(ncol=3, nrow=0)
+lm.est.vals <- matrix(ncol=2, nrow=0)
+lm.true.vals <- matrix(ncol=2, nrow=0)
 for (rep in sequence(nreps)) {
   a<-runif(1, -1, 3)
   b<-runif(1, -3, 3)
   c<-runif(1, 0.000, 0.01)
-  true.vals <- rbind(true.vals, c(a, b, c)) #store the values of a,b,c in true.vals' rows
-  true.logphi <- runif(ngenes, -6, -1) #values for log(g*phi)
+  true.vals <- rbind(true.vals, c(a, b, c))
+  true.logphi <- runif(ngenes, -6, -1)
   est.logphi <- rnorm(length(true.logphi), true.logphi, phi.sd)
   est.logphig <- rnorm(length(true.logphi), a+(b+1)*true.logphi, sqrt(c))
   recovered.vals <- optim(c(1,1,1), fn=LikelihoodForOptim, logphi.vector=est.logphi, logphig.vector=est.logphig)$par
   recovered.vals[3] <- exp(recovered.vals[3]) #optimize in log space, convert back
   est.vals <- rbind(est.vals, recovered.vals)  
   print(c(true.vals[rep,], est.vals[rep,]))
+  lm.est <- lm(est.logphig~est.logphi)
+  lm.true <- lm(est.logphig~true.logphi)
+  lm.est.vals <- rbind(lm.est.vals,as.numeric(lm.est$coefficients))
+  lm.true.vals <- rbind(lm.true.vals,as.numeric(lm.true$coefficients))
+  print(lm.est$coefficients)
+  print(lm.true$coefficients)
 }
 par(mfcol=c(1,3))
 for (trait in sequence(3)) {
