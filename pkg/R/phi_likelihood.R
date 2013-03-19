eac27c33ca3431454aa0877cb41217608355ccdb
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-19 14:09:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@391 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/phi_likelihood.R
+++ pkg/R/phi_likelihood.R
@@ -1,46 +1,53 @@
-lik_phi <- function(phi_ij,phi_hat,sigma,sigma_hat){ 
-  #switch sigma and sigma_hat doesn't change the result
-  integrand <- function(phi_i)
-    #(1/(2*pi*sigma*sigma_hat))*exp(-(phi_ij-phi_i)^2/(2*sigma^2)-(phi_i-phi_hat)^2/(2*sigma_hat^2))
-    dnorm(phi_ij,mean=phi_i,sd=sigma)*dnorm(phi_i,mean=phi_hat,sd=sigma_hat)
-  integrate(integrand,-Inf,Inf)$value
-}
-
-lik_phi_vec <- Vectorize(lik_phi,vectorize.args="phi_ij")
-optim.phi.sigma <- function(phis,phi_hat,sigma,sigma_hat,maxeval="500",print_level=1, ...){
-  ## a function of x that return the sum of -loglikelihood values for all genes with s optimized separately for different genes
-  ab <- c(phi_hat,sigma,sigma_hat)
-  fn <- function(ab,phis){
-    cat(ab,"\n")
-    loglik <- -sum(log(lik_phi_vec(phis,ab[1],ab[2],ab[3])))
-    return(loglik) #summation of all values
-  }
-  
-  lower <- c(-Inf,0.01,0.01) #lower bound
-  upper <- c(Inf,Inf,Inf) #upper bound
-  #options for optimizer
-  opts <- list("algorithm"="NLOPT_LN_SBPLX","maxeval"=maxeval,
-               "xtol_rel"=1e-6,"ftol_rel"=.Machine$double.eps,"print_level"=print_level)
-  res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,phis=phis)
-  return(res)
-}
-
-lik_gphi <- function(gphi,phi,sigma,a,b,sigma_e){
+# lik_phi <- function(phi_ij,phi_hat,sigma,sigma_hat){ 
+#   #switch sigma and sigma_hat doesn't change the result
+#   integrand <- function(phi_i)
+#     #(1/(2*pi*sigma*sigma_hat))*exp(-(phi_ij-phi_i)^2/(2*sigma^2)-(phi_i-phi_hat)^2/(2*sigma_hat^2))
+#     dnorm(phi_ij,mean=phi_i,sd=sigma)*dnorm(phi_i,mean=phi_hat,sd=sigma_hat)
+#   integrate(integrand,-Inf,Inf)$value
+# }
+# 
+# lik_phi_vec <- Vectorize(lik_phi,vectorize.args="phi_ij")
+# optim.phi.sigma <- function(phis,phi_hat,sigma,sigma_hat,maxeval="500",print_level=1, ...){
+#   ## a function of x that return the sum of -loglikelihood values for all genes with s optimized separately for different genes
+#   ab <- c(phi_hat,sigma,sigma_hat)
+#   fn <- function(ab,phis){
+#     cat(ab,"\n")
+#     loglik <- -sum(log(lik_phi_vec(phis,ab[1],ab[2],ab[3])))
+#     return(loglik) #summation of all values
+#   }
+#   
+#   lower <- c(-Inf,0.01,0.01) #lower bound
+#   upper <- c(Inf,Inf,Inf) #upper bound
+#   #options for optimizer
+#   opts <- list("algorithm"="NLOPT_LN_SBPLX","maxeval"=maxeval,
+#                "xtol_rel"=1e-6,"ftol_rel"=.Machine$double.eps,"print_level"=print_level)
+#   res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,phis=phis)
+#   return(res)
+# }
+##likelihood function of log(g*phi), 
+##given mean, variance value of phi, a, b, and variance of error sigma_e
+lik_gphi <- function(gphi,phi,a,b,sigma,sigma_e){
   dnorm(gphi,mean=a+(b+1)*phi,sd=sqrt(((b+1)^2*sigma^2)+sigma_e^2))
 }
 lik_gphi_vec <- Vectorize(lik_gphi,vectorize.args=c("gphi","phi"))
-optim.ab.sigma_e <- function(gphis,phis, a,b,sigma,sigma_e,maxeval="500",print_level=1){
+
+lik_gphi_fix <- function(gphi,phi,a,b,c){
+  dnorm(gphi,mean=a+(b+1)*phi,sd=c)
+}
+lik_gphi_fix_vec <- Vectorize(lik_gphi_fix,vectorize.args=c("gphi","phi"))
+
+optim.ab.sigma_e <- function(gphis,phis, a,b,c,maxeval="500",print_level=1){
   ## a function of x that return the sum of -loglikelihood values for all genes with s optimized separately for different genes
-  ab <- c(a,b,log(sigma_e))
+  ab <- c(a,b,log(c))
   fn <- function(ab){
-    cat(ab,"\n")
-    loglik <- -sum(log(lik_gphi_vec(gphi=gphis,phi=phis,sigma=sigma,a=ab[1],b=ab[2],
-                                    sigma_e=exp(ab[3]))))
+    cat(ab[1],ab[2],exp(ab[3]),"\n")
+    loglik <- -sum(log(lik_gphi_fix_vec(gphi=gphis,phi=phis,a=ab[1],b=ab[2],
+                                    c=exp(ab[3]))))
     return(loglik) #summation of all values
   }
   
-  lower <- c(-100,-100,-Inf) #lower bound
-  upper <- c(100,100,Inf) #upper bound
+  lower <- c(-Inf,-Inf,-Inf) #lower bound
+  upper <- c(Inf,Inf,Inf) #upper bound
   #options for optimizer
   opts <- list("algorithm"="NLOPT_LN_SBPLX","maxeval"=maxeval,
                "xtol_rel"=1e-6,"ftol_rel"=.Machine$double.eps,"print_level"=print_level)
@@ -50,3 +57,16 @@ optim.ab.sigma_e <- function(gphis,phis, a,b,sigma,sigma_e,maxeval="500",print_l
 
 #ps1 <- optim.phi.sigma(allphi,1,1,1)
 #absigma <- optim.ab.sigma_e(lgphi,a=0,b=0,phi_hat=ps1$solution[1],sigma_hat=ps1$solution[3],sigma_e=1)
+nreps<-20
+ngenes<-10600
+true.vals<-matrix(ncol=3, nrow=0)
+est.vals<-matrix(ncol=3, nrow=0)
+phi.sd <- 0.05
+phig.sd <- 0.01
+a<-runif(1, -1, 3)
+b<-runif(1, -3, 3)
+c<-runif(1, 0.000, 0.01)
+true.vals <- rbind(true.vals, c(a, b, c))
+true.logphi <- runif(ngenes, -6, -1)
+est.logphi <- rnorm(length(true.logphi), true.logphi, phi.sd)
+est.logphig <- rnorm(length(true.logphi), a+(b+1)*true.logphi, sqrt(c))
