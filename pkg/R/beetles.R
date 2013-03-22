81e89f34a6ac3200912d167512ec12bed8af2a5e
chaij (JJ Chai) <chaij@umail.iu.edu>
2013-03-22 15:51:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@404 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/beetles.R
+++ pkg/R/beetles.R
@@ -1,10 +1,10 @@
 ## beetles data analysis
 ## partitions: 
 ## AATS: 1-915
-## CAD: 916-2973
-## TPI: 2963-3460
-## PGD: 3461-4264
-## SNF: 4265-4825
+## CAD: 916-2962
+## TPI: 2963-3460 
+## PGD: 3461-4264 
+## SNF: 4265-4825 
 ## RNA POL II: 4826-5725
 ## EF-1alpha-1,2,3pos: 5726-6783
 ## 18S: 6784-10478
