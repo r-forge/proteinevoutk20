50673164d603d9256b3e41d254f36d71a9afefef
chaij (JJ Chai) <chaij@umail.iu.edu>
2013-03-28 18:39:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@411 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/beetles.R
+++ pkg/R/beetles.R
@@ -10,8 +10,13 @@
 ## 18S: 6784-10478
 ## 28S: 10479-12777
 source("~/proteinevoutk20/pkg/R/main.R")
-bedata <- conv("~/proteinevoutk20/pkg/Data/beetles.fasta",type="phyDat")
-betree <- read.nexus("~/proteinevoutk20/pkg/Data/beetle_ml.tre")
+#beetle data without 18S and 28S, 28 taxa from earlier paper
+beetle <- read.nexus.data("~/proteinevoutk20/pkg/Data/beetle_short_data.nex")
+#beetle tree, 28 taxa
+betree <- read.nexus("~/proteinevoutk20/pkg/Data/beetle_short.nex")
+betree$edge.length <- rep(0.001,56)
+pos <- 1:915
+bedata <- conv(beetle,range=pos,type="phyDat")
 be.res <- mllm1(bedata,betree,s=1,beta=be,gamma=ga)
 be.optim <- optim.mllm1(be.res,optQ=T,optBranch=T,optsWeight=T,
                         control=list(epsilon=1e-08,hmaxit=30,htrace=TRUE,print_level=0,maxit=100))
