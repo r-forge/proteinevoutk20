fa7a6f6b93767062a03fb97851da5bf9c4a58b9e
chaij (JJ Chai) <chaij@umail.iu.edu>
2013-04-22 16:04:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@451 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/beetle_collect.R
+++ pkg/R/beetle_collect.R
@@ -1,22 +1,22 @@
-#maxdir <- "~/BackupProEvo/Newton/beetle/tree1/"
-maxdir <- "~/BackupProEvo/Newton/beetle/tree2/"
+maxbdir <- "~/BackupProEvo/Newton/beetle/tree1/"
+#maxbdir <- "~/BackupProEvo/Newton/beetle/tree2/"
 l <- 8
-res_max <- vector("list",length=8)
+res_maxb <- vector("list",length=8)
 
 for(genect in 1:l){
-  filename = paste(maxdir, "bee_",genect,".RData",sep="")
+  filename = paste(maxbdir, "bee_",genect,".RData",sep="")
   if(!file.exists(filename))
     cat("load RData for gene", genect,"failed, file does not exist","\n")
   else{
     load(filename)
-    res_max[[genect]] <- bee.optim
+    res_maxb[[genect]] <- bee.optim
   }
 }
 
-s_max <- sapply(1:8,function(x) res_max[[x]]$s)
-loglik_max <- sapply(1:8,function(x) res_max[[x]]$ll$loglik)
-GM_max <- sapply(1:8,function(x) res_max[[x]]$GMweights)
-Q_max <- sapply(1:8,function(x) res_max[[x]]$Q)
-br_max <- sapply(1:8,function(x) sum(res_max[[x]]$tree$edge.length))
+s_maxb <- sapply(1:8,function(x) res_maxb[[x]]$s)
+loglik_maxb <- sapply(1:8,function(x) res_maxb[[x]]$ll$loglik)
+GM_maxb <- sapply(1:8,function(x) res_maxb[[x]]$GMweights)
+Q_maxb <- sapply(1:8,function(x) res_maxb[[x]]$Q)
+br_maxb <- sapply(1:8,function(x) sum(res_maxb[[x]]$tree$edge.length))
 
-#sw_max <- rbind(s_max,GM_max[2,],GM_max[3,])
+#sw_maxb <- rbind(s_maxb,GM_maxb[2,],GM_maxb[3,])
--- pkg/R/prunetree.R
+++ pkg/R/prunetree.R
@@ -1,7 +1,7 @@
-load("~/BackupProEvo/Lab9/prunetree/gene1.RData")
+load("~/BackupProEvo/Lab9/prunetree/gene83.RData")
 source("~/proteinevoutk20/pkg/R/main.R")
 source("~/proteinevoutk20/pkg/R/simulation.R")
-nsim <- 2
+nsim <- 20
 wsim <- vector(mode="list",length=nsim)
 wsimWag <- vector(mode="list",length=nsim)
 wroots <- vector(mode="list",length=nsim)
@@ -14,15 +14,16 @@ for(i in 1:nsim){
   mumat = aa_MuMat_form(res_op$Q)
   sim <- simulation(root,opaa,t=brlen,s=s,DisMat=dismat,MuMat=mumat,bfaa=bfaa) #simulation     
   wsim[[i]] <- sim.info(sim,opaa=opaa,s=s,beta=beta,gamma=gamma)
-  simwag <- simAA(rootseq=root,bf=bfaa,model="LG",inv=0,rate=0.771,k=4)
+  simwag <- simAA(rootseq=root,bf=bfaa,model="mtArt",inv=0,rate=0.621,k=4)
   wsimWag[[i]] <- sim.info(simwag$seq,opaa=opaa,s=s,beta=beta,gamma=gamma)
   #wsimWag[[i]] <- sim.Wag(t=brlen,protein=root,bf=bfaa,opaa=opaa,s=s,dismat=dismat)
 }
 ##################################################################################################
 ## analysis of the simulated data under both models
 ##################################################################################################
-pdf("~/proteinevoutk20/pkg/Plot/prunetree/wgene1.pdf")
+#pdf("~/proteinevoutk20/pkg/Plot/prunetree/wgene1.pdf")
 ##functionalities based on the parameters estimated earlier
+par(mfrow=c(2,3))
 wstart.ftny.vec <-  sapply(1:nsim,function(i) head(wsim[[i]]$fty,1))
 wend.ftny.vec <- sapply(1:nsim,function(i) tail(wsim[[i]]$fty,1))
 plot.density(density(wend.ftny.vec),xlab="Functionality",ylab="density",main="Our model")
@@ -48,5 +49,29 @@ abline(v=mean(pchem_d(datanum[6,],opaa,DisMat=dismat)))
 wend.dis.obs.vec.wag <- sapply(1:nsim,function(x) mean(pchem_d(protein1=as.numeric(tail(wsimWag[[x]]$sim[,1:length(index)],1)),protein2=datanum[6,],DisMat=GM)))
 plot.density(density(wend.dis.obs.vec.wag),xlab="Avg Distance from observed seq",ylab="density",main="WAG model")
 ##################################################################################################
-dev.off()
-save.image(file="wgene1.RData",compress=TRUE)
+#dev.off()
+#save.image(file="wgene1.RData",compress=TRUE)
+par(mfrow=c(1,2))
+## plot the functionalities of protein sequence on the simulation path
+wsim.fty.range <- range(unlist(sapply(1:nsim,function(x) wsim[[x]]$fty)))
+wsimWag.fty.range <- range(unlist(sapply(1:nsim,function(x) wsimWag[[x]]$fty)))
+fty.range <- range(c(unlist(sapply(1:nsim,function(x) wsim[[x]]$fty)),unlist(sapply(1:nsim,function(x) wsimWag[[x]]$fty))))
+plot(wsim[[1]]$ftyfun,xlab="time",ylab="functionality",main=paste("functionality, s=",round(s,3),sep=""),pch=20,xlim=c(0,brlen),xaxs="i",ylim=fty.range)
+for(i in 2:nsim){
+  plot(wsim[[i]]$ftyfun,pch=20,xlim=c(0,brlen),xaxs="i",ylim=fty.range,add=TRUE)
+}
+for(i in 1:nsim){
+  plot(wsimWag[[i]]$ftyfun,pch=20,xlim=c(0,brlen),xaxs="i",ylim=fty.range,col="red",add=TRUE)
+}
+## plot the distances of protein sequence from optimal protein on the simulation path
+wsim.dis.range <- range(unlist(sapply(1:nsim,function(x) wsim[[x]]$dis)))
+wsimWag.dis.range <- range(unlist(sapply(1:nsim,function(x) wsimWag[[x]]$dis)))
+dis.range <- range(c(unlist(sapply(1:nsim,function(x) wsim[[x]]$dis)),unlist(sapply(1:nsim,function(x) wsimWag[[x]]$dis))))
+plot(wsim[[1]]$disfun,xlab="time",ylab="distance",main=paste("distance, s=",round(s,3),sep=""),pch=20,xlim=c(0,brlen),xaxs="i",ylim=dis.range)
+for(i in 2:nsim){
+  plot(wsim[[i]]$disfun,pch=20,xlim=c(0,brlen),xaxs="i",ylim=dis.range,add=TRUE)
+}
+for(i in 1:nsim){
+  plot(wsimWag[[i]]$disfun,pch=20,xlim=c(0,brlen),xaxs="i",ylim=dis.range,col="red",add=TRUE)
+}
+
