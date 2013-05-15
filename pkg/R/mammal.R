33b6106e2c5a8dd6af1cdbf7f3709db9de7a88f0
chaij (JJ Chai) <chaij@umail.iu.edu>
2013-05-15 19:49:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@486 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mammal.R
+++ pkg/R/mammal.R
@@ -2,14 +2,14 @@ source("~/proteinevoutk20/pkg/R/prune.R")
 gene = 2
 fastafile <- "~/proteinevoutk20/pkg/Data/mammals/mam15.fasta"
 load("~/proteinevoutk20/pkg/Data/mammals/charset.RData") 
-mamtree <- read.nexus("proteinevoutk20/pkg/Data/mammals/T15taxa.nex")
+mamtree <- read.nexus("~/proteinevoutk20/pkg/Data/mammals/T15taxa.nex")
 prottestfile <- paste("~/proteinevoutk20/pkg/Result/Prottest/mammal/mam15_",gene,"_prottest.txt",sep="")
 RDatafile <- paste("gene",gene,".RData",sep="")
 best_emp_model <- get_best_model(prottestfile)
 dtip = 12
 p2 <- prune_emp(fastafile,dtip,mamtree,best_emp_model$model,range=charset[[gene]])
-p1 <- prune_new(fastafile,dtip,mamtree,ancestral="max",range=charset[[gene]])
-save.image(RDatafile,compress=TRUE)
+# p1 <- prune_new(fastafile,dtip,mamtree,ancestral="max",range=charset[[gene]])
+# save.image(RDatafile,compress=TRUE)
 ##truncate the names of tip so that the length is no bigger than 10 for prottest
 ## the resulting tree is written in file mam15.tre
 ## mamtree$tip.label <- sapply(1:15, function(x) substr(mamtree$tip.label[x],1,10))
