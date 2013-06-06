9886510fb27c0ddacac267d2db66ffdfc6d15c83
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-06 17:02:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@515 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test.R
+++ pkg/R/test.R
@@ -1,13 +1,19 @@
 rm(list=ls())
-gene = 1
-RDatafile <- paste("~/BackupProEvo/Newton/rokas7/simtree/simMax/gene",gene,".RData",sep="")
+gene = 20
+RDatafile <- paste("~/BackupProEvo/Newton/rokas7/simAna/Eqm/gene",gene,".RData",sep="")
 load(RDatafile)
 source("~/proteinevoutk20/pkg/R/prune.R")
 source("~/proteinevoutk20/pkg/R/plot_simtree.R")
 ftny.vec <- apply(datanum,MARGIN=1,FUN=Ftny_protein,protein_op=opaa[index],s=s,DisMat=GM_cpv(GM_CPV,al,beta,gamma))
 ## find the starting and ending position of all edges in the tree
 #plot_trace(sim=sim$emp[[1]],plottip=FALSE,plotftny=TRUE,plotdis=FALSE)
-plot_trace(sim=sim$new[[1]],plottip=TRUE,plotftny=TRUE,plotdis=FALSE)
+plot_trace(sim=sim,plottip=TRUE,plotftny=TRUE,plotdis=FALSE)
 #################################################
 #plot_trace(sim=sim$emp[[1]],plottip=FALSE,plotftny=FALSE,plotdis=TRUE)
-plot_trace(sim=sim$new[[1]],plottip=TRUE,plotftny=FALSE,plotdis=TRUE)
+plot_trace(sim=sim,plottip=TRUE,plotftny=FALSE,plotdis=TRUE)
+# s = 5
+# fixmatall <- fixmatAll(s,DisMat=dismat)
+# Qall = QAllaa1(fixmatall,mumat)
+# bf <- sapply(Qall,FUN=eqmQ)
+# for(i in 1:20)
+#   plot(sort(bf[,i],decreasing=TRUE),type="s",ylim=c(0,1))
