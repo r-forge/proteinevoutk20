2abb1f64b994a997639b4ad222451c9852404090
chaij <chaij@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-05 14:59:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/proteinevoutk20@539 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.R
+++ pkg/R/plot.R
@@ -1,128 +1,128 @@
-# library("qgraph")
-# library("network")
-# ##set all positive entries in mumat to 1, and others to 0, save it in mumat01
-# net <- network(MUMAT,loops=F)
-# mumat01 <- net[,]
-# 
-# #view the color transition in stripes
-# Col <- rev(heat.colors(20))
-# image(1:20,1,as.matrix(1:20),col=Col)
-# ##############################################################
-# ##for a network object, find the step 1 neighbors and step 2 neighbors of node k
-# ##return as a list with 2 entries: ng1, ng2
-# get.neighbor.step <- function(net,k){
-#   ng1 <- get.neighborhood(net,k) # 1-step neighbors of node k
-#   ng2 <- NULL # 2-step neighbors
-#   for(i in ng1){
-#     ngi <- get.neighborhood(net,i) # 1-step neighbors of neighbors of k
-#     ng2_add <- setdiff(ngi,c(ng1,k,ng2)) # 2-step neighbors of k only
-#     ng2 <- c(ng2,ng2_add) # put 2-step neighbors together
-#   }
-#   return(list(step1=ng1,step2=ng2))
-# }
-# ##############################################################
-# ##Given an integer, find the coordinates of that many points
-# spacing <- function(k){
-#   if(k%%2!=0){
-#     return((-(k-1)/2):((k-1)/2))
-#   }
-#   if(k%%2==0){
-#     return(setdiff((-k/2):(k/2),0))
-#   }
-# }
-# ##Create a layout for graph according to the neighborhood status of nodes in a network
-# create.layout <- function(op,ng1,ng2){
-#   layout <- matrix(0,20,2)
-#   layout[op,] <- c(0,1)
-#   layout[ng1,] <- cbind(spacing(length(ng1)),0.5)
-#   layout[ng2,] <- cbind(spacing(length(ng2)),-0.5)
-#   return(layout)
-# }
-# ##############################################################
-# ##build a matrix with entries as difference between functionalities for corresponding nodes
-# ##to be used for graphing
-# ##if one state/node i is better than another j in functionality, then there is an arrow going from j to i
-# ##i.e. if fmatrix[j,i] > 0, keep the value, otherwise, fmatrix[j,i] = 0
-# fmatrix <- function(ftny){
-#   res <- matrix(0,20,20)
-#   for(i in 1:20){
-#     for(j in 1:20){
-#       res[i,j] <- max(ftny[j] - ftny[i],0)
-#     }
-#   }
-#   return(res)
-# }
-# ##############################################################
-# ##functionality qgraph
-# ## parameters: op - optimal amino acid; s - selection coefficient
-# fgraph <- function(op,s,beta=be,gamma=ga,graph=TRUE){
-#   ftny <- vector("numeric",length=20)
-#   GM <- GM_cpv(GM_CPV,al,beta,gamma)
-#   #functionality vector
-#   for(i in 1:20){
-#     ftny[i] <- Ftny_protein(i,op,s,GM)
-#   }
-#   ra <- rank(ftny) #rank of the functionality, used later for coloring of nodes
-#   f.matrix <- fmatrix(ftny) #matrix with differences between functionalities
-#   ng <- get.neighbor.step(net,op) # find the 1- and 2- step neighbors
-#   ng1 <- ng$step1
-#   ng2 <- ng$step2
-#   lay <- create.layout(op,ng1,ng2) # layout of the figure
-#   ##find out the out degrees (number of outflows) from each amino acid
-#   #the matrix that's plotted, there is edge if it's possible to mutate 
-#   #and only goes to higher functionality
-#   fm.matrix <- f.matrix*mumat01
-#   #number of arrows going out of the states
-#   # if 0 then it'll be a local optimum
-#   out.num <- vector("numeric",20) 
-#   for(i in 1:20){
-#   out.num[i] <- length(which(fm.matrix[i,]!=0))
-#   }
-#   num.localop <- sum(out.num==0)
-#   if(graph){
-#     ##If the outflow for a node is 0, use square for the node, otherwise circle
-#     node.shape <- vector(mode="character",20)
-#     node.shape <- ifelse(out.num==0,"square","circle")
-#     qgraph(f.matrix*mumat01,color=Col[ra],curve=0.3,cut=0.001,
-#            layout=lay,asize=0.15,shape=node.shape)
-#            
-#            ##filetype="pdf",filename=paste("A_op",op,sep=""))
-#   }
-#   return(list(out=out.num,num.localop=num.localop))
-#   
-# }
-# 
-# fgraph1 <- function(op,s,beta=be,gamma=ga,graph=TRUE){
-#   ftny <- vector("numeric",length=20)
-#   GM <- GM_cpv(GM_CPV,al,beta,gamma)
-#   #functionality vector
-#   for(i in 1:20){
-#     ftny[i] <- Ftny_protein(i,op,s,GM)
-#   }
-#   ra <- rank(ftny) #rank of the functionality, used later for coloring of nodes
-#   f.matrix <- fmatrix(ftny) #matrix with differences between functionalities
-#   ##find out the out degrees (number of outflows) from each amino acid
-#   #the matrix that's plotted, there is edge if it's possible to mutate 
-#   #and only goes to higher functionality
-#   fm.matrix <- f.matrix*mumat01
-#   #number of arrows going out of the states
-#   # if 0 then it'll be a local optimum
-#   out.num <- vector("numeric",20) 
-#   for(i in 1:20){
-#     out.num[i] <- length(which(fm.matrix[i,]!=0))
-#   }
-#   num.localop <- sum(out.num==0)
-#   if(graph){
-#     ##If the outflow for a node is 0, use square for the node, otherwise circle
-#     node.shape <- vector(mode="character",20)
-#     node.shape <- ifelse(out.num==0,"square","circle")
-#     qgraph(f.matrix*mumat01,color=Col[ra],cut=0.001,
-#            asize=0.15,shape=node.shape)
-#     
-#     ##filetype="pdf",filename=paste("A_op",op,sep=""))
-#   }
-#   return(list(out=out.num,num.localop=num.localop))
-# }
+library("qgraph")
+library("network")
+##set all positive entries in mumat to 1, and others to 0, save it in mumat01
+net <- network(mumat,loops=F)
+mumat01 <- net[,]
+
+#view the color transition in stripes
+Col <- rev(heat.colors(20))
+image(1:20,1,as.matrix(1:20),col=Col)
+##############################################################
+##for a network object, find the step 1 neighbors and step 2 neighbors of node k
+##return as a list with 2 entries: ng1, ng2
+get.neighbor.step <- function(net,k){
+  ng1 <- get.neighborhood(net,k) # 1-step neighbors of node k
+  ng2 <- NULL # 2-step neighbors
+  for(i in ng1){
+    ngi <- get.neighborhood(net,i) # 1-step neighbors of neighbors of k
+    ng2_add <- setdiff(ngi,c(ng1,k,ng2)) # 2-step neighbors of k only
+    ng2 <- c(ng2,ng2_add) # put 2-step neighbors together
+  }
+  return(list(step1=ng1,step2=ng2))
+}
+##############################################################
+##Given an integer, find the coordinates of that many points
+spacing <- function(k){
+  if(k%%2!=0){
+    return((-(k-1)/2):((k-1)/2))
+  }
+  if(k%%2==0){
+    return(setdiff((-k/2):(k/2),0))
+  }
+}
+##Create a layout for graph according to the neighborhood status of nodes in a network
+create.layout <- function(op,ng1,ng2){
+  layout <- matrix(0,20,2)
+  layout[op,] <- c(0,1)
+  layout[ng1,] <- cbind(spacing(length(ng1)),0.5)
+  layout[ng2,] <- cbind(spacing(length(ng2)),-0.5)
+  return(layout)
+}
+##############################################################
+##build a matrix with entries as difference between functionalities for corresponding nodes
+##to be used for graphing
+##if one state/node i is better than another j in functionality, then there is an arrow going from j to i
+##i.e. if fmatrix[j,i] > 0, keep the value, otherwise, fmatrix[j,i] = 0
+fmatrix <- function(ftny){
+  res <- matrix(0,20,20)
+  for(i in 1:20){
+    for(j in 1:20){
+      res[i,j] <- max(ftny[j] - ftny[i],0)
+    }
+  }
+  return(res)
+}
+##############################################################
+##functionality qgraph
+## parameters: op - optimal amino acid; s - selection coefficient
+fgraph <- function(op,s,beta=be,gamma=ga,graph=TRUE){
+  ftny <- vector("numeric",length=20)
+  GM <- GM_cpv(GM_CPV,al,beta,gamma)
+  #functionality vector
+  for(i in 1:20){
+    ftny[i] <- Ftny_protein(i,op,s,GM)
+  }
+  ra <- rank(ftny) #rank of the functionality, used later for coloring of nodes
+  f.matrix <- fmatrix(ftny) #matrix with differences between functionalities
+  ng <- get.neighbor.step(net,op) # find the 1- and 2- step neighbors
+  ng1 <- ng$step1
+  ng2 <- ng$step2
+  lay <- create.layout(op,ng1,ng2) # layout of the figure
+  ##find out the out degrees (number of outflows) from each amino acid
+  #the matrix that's plotted, there is edge if it's possible to mutate 
+  #and only goes to higher functionality
+  fm.matrix <- f.matrix*mumat01
+  #number of arrows going out of the states
+  # if 0 then it'll be a local optimum
+  out.num <- vector("numeric",20) 
+  for(i in 1:20){
+  out.num[i] <- length(which(fm.matrix[i,]!=0))
+  }
+  num.localop <- sum(out.num==0)
+  if(graph){
+    ##If the outflow for a node is 0, use square for the node, otherwise circle
+    node.shape <- vector(mode="character",20)
+    node.shape <- ifelse(out.num==0,"square","circle")
+    qgraph(f.matrix*mumat01,color=Col[ra],curve=0.3,cut=0.001,
+           layout=lay,asize=0.15,shape=node.shape)
+           
+           ##filetype="pdf",filename=paste("A_op",op,sep=""))
+  }
+  return(list(out=out.num,num.localop=num.localop))
+  
+}
+
+fgraph1 <- function(op,s,beta=be,gamma=ga,graph=TRUE){
+  ftny <- vector("numeric",length=20)
+  GM <- GM_cpv(GM_CPV,al,beta,gamma)
+  #functionality vector
+  for(i in 1:20){
+    ftny[i] <- Ftny_protein(i,op,s,GM)
+  }
+  ra <- rank(ftny) #rank of the functionality, used later for coloring of nodes
+  f.matrix <- fmatrix(ftny) #matrix with differences between functionalities
+  ##find out the out degrees (number of outflows) from each amino acid
+  #the matrix that's plotted, there is edge if it's possible to mutate 
+  #and only goes to higher functionality
+  fm.matrix <- f.matrix*mumat01
+  #number of arrows going out of the states
+  # if 0 then it'll be a local optimum
+  out.num <- vector("numeric",20) 
+  for(i in 1:20){
+    out.num[i] <- length(which(fm.matrix[i,]!=0))
+  }
+  num.localop <- sum(out.num==0)
+  if(graph){
+    ##If the outflow for a node is 0, use square for the node, otherwise circle
+    node.shape <- vector(mode="character",20)
+    node.shape <- ifelse(out.num==0,"square","circle")
+    qgraph(f.matrix*mumat01,color=Col[ra],cut=0.001,
+           asize=0.15,shape=node.shape)
+    
+    ##filetype="pdf",filename=paste("A_op",op,sep=""))
+  }
+  return(list(out=out.num,num.localop=num.localop))
+}
 # ##############################################################
 # ## given beta, gamma, and s, find the number of local optima for all optimal amino acids
 # ## return a vector of length 20
@@ -152,78 +152,78 @@
 # points(GM_maj[3,],col="blue")
 
 ##############################################################################
-# plot simulated data, using gene number
-source("~/proteinevoutk20/pkg/R/rokas_collect.R") #collect results on genes, using max/maj rule
-genenum <- 72
-datafile = paste("~/BackupProEvo/Newton/rokas_max/gene",genenum,"_s_weight.RData",sep="")
-load(datafile)
-source("~/proteinevoutk20/pkg/R/main.R")
-data = res_op$data
-index = attr(data,"index")
-opaa = res_op$ll$opaa[index]
-bfaa = findBf2(data)
-root = sample(20,length(index),replace=T,prob=bfaa) #sample root sequence
-s = res_op$s
-beta = res_op$GMweights[2]
-gamma = res_op$GMweights[3]
-dismat = GM_cpv(GM_CPV,al,beta,gamma)
-mumat = aa_MuMat_form(res_op$Q)
-plot.sim <- function(s=1,t=10,root=root,opaa=opaa,beta,gamma,bfaa=bfaa,func=TRUE,dist=TRUE, add=FALSE){
-   dismat = GM_cpv(GM_CPV,al,beta,gamma)
-   mumat = aa_MuMat_form(res_op$Q)
-  sim <- simulation(root,opaa,t=t,s=s,DisMat=dismat,MuMat=mumat,bfaa=bfaa) #simulation
-  l <- dim(sim)[2] - 2
-  fty <- apply(sim[,1:l],MARGIN=1,FUN=Ftny_protein,protein_op=opaa,s=s,DisMat=dismat)#functionality
-  dis <- apply(sim[,1:l],MARGIN=1,FUN=pchem_d,protein2=opaa,DisMat=dismat)#distance from optimal amino acids
-  dis <- apply(dis,2,sum)#sum of distances for all sites
-  ftyfun <- stepfun(sim[-1,l+1],fty,f=0,right=FALSE)#make step functions
-  disfun <- stepfun(sim[-1,l+1],dis,f=0,right=FALSE)
-  if(func) #plot functionality vs time
-    plot(ftyfun,xlab="time",ylab="functionality",main=paste("functionality, s=",round(s,3),sep=""),pch=20,xlim=c(0,t),xaxs="i",add=add)
-  if(dist) #plot distance vs time
-    plot(disfun,xlab="time",ylab="distance",main=paste("distance, s=",round(s,3),sep=""),pch=20,xlim=c(0,t),xaxs="i",add=add)
-  #return(as.numeric(tail(sim,1)[1:l])) #the sequence at the end of simulation
-   return(list(sim=sim,fty=fty,dis=dis,ftyfun=ftyfun,disfun=disfun)) #store the simulation result for later use
-}
-sim <- plot.sim(s=s,t=br_max[genenum],root=root,opaa=opaa,beta,gamma,func=TRUE,dist=TRUE)
-
-plot(sim83$ftyfun,xlab="time",ylab="functionality",main="Functionality",pch=20,xlim=c(0,br_max[83]),ylim=c(0.15,0.8),xaxs="i",col="blue")
-plot(sim72$ftyfun,pch=20,xaxs="i",add=TRUE,col="red")
-legend(5,y=0.7,legend=c("g=1.10, length=417","g=5.49, length=309"),col=c("blue","red"),lty=1,bty="n")
-
-plot(sim83$disfun,xlab="time",ylab="distance",main="Distance from optima",pch=20,xlim=c(0,br_max[83]),ylim=c(0,400),xaxs="i",col="blue")
-plot(sim72$disfun,pch=20,xaxs="i",add=TRUE,col="red")
-legend(5,y=350,legend=c("g=1.10, length=417","g=5.49, length=309"),col=c("blue","red"),lty=1,bty="n")
-
-plot.w <- function(x,add=FALSE){
-  plot(wsim[[x]]$ftyfun,xlab="time",ylab="functionality",ylim=c(0.2,0.8),main=paste("functionality, s=",round(s,3),sep=""),do.points=FALSE,xlim=c(0,brlen),xaxs="i",add=add)
-  plot(wsimWag[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
-  plot(sim[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE)
-  plot(simWag[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
-}
-plot(wsim[[1]]$ftyfun,xlab="time",ylab="functionality",ylim=c(0,1),main=paste("functionality, s=",round(s,3),sep=""),do.points=FALSE,xlim=c(0,brlen),xaxs="i")
-plot(wsimWag[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
-plot(sim[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE)
-plot(simWag[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
-
-plot.func <- function(gene_num,count=3,margin=0.1){
-  #filename <- paste("~/BackupProEvo/Lab9/prunetree/gene",gene_num,".RData",sep="")
-  wfilename <- paste("~/BackupProEvo/Lab9/Wagprunetree/wgene",gene_num,".RData",sep="") #RData file with simulated data
-  #load(filename)
-  load(wfilename)
-  obs.fty <- Ftny_protein(protein=datanum[6,],protein_op=opaa,s=s,DisMat=dismat)
-  ## find the limits for y-axis
-  fty_lim <- rbind(range(wsimWag[[1]]$fty),range(wsim[[1]]$fty),range(sim[[1]]$fty),range(simWag[[1]]$fty),rep(obs.fty,2)) ##ranges of functionalities for several typical simulations
-  ylim <- c(min(fty_lim[,1]),max(fty_lim[,2])) #lower and upper bound
-  ylim <- c(ylim[1]-ylim[1]*margin,ylim[2]+ylim[2]*margin) #extend them with a little margin
-  
-  plot(sim[[1]]$ftyfun,do.points=FALSE,xaxs="i",col="red",xlab="time",ylab="functionality",ylim=ylim,xlim=c(0,brlen),
-       main=paste("gene",gene_num,",  s=",round(s_max[gene_num],2),sep="")) #start a plot
-  abline(h=obs.fty,col="green",lwd=2)
-  for(i in 1:count){
-    plot(sim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
-    plot(simWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
-    plot(wsim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
-    plot(wsimWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
-  }
-}
+# # plot simulated data, using gene number
+# source("~/proteinevoutk20/pkg/R/rokas_collect.R") #collect results on genes, using max/maj rule
+# genenum <- 72
+# datafile = paste("~/BackupProEvo/Newton/rokas_max/gene",genenum,"_s_weight.RData",sep="")
+# load(datafile)
+# source("~/proteinevoutk20/pkg/R/main.R")
+# data = res_op$data
+# index = attr(data,"index")
+# opaa = res_op$ll$opaa[index]
+# bfaa = findBf2(data)
+# root = sample(20,length(index),replace=T,prob=bfaa) #sample root sequence
+# s = res_op$s
+# beta = res_op$GMweights[2]
+# gamma = res_op$GMweights[3]
+# dismat = GM_cpv(GM_CPV,al,beta,gamma)
+# mumat = aa_MuMat_form(res_op$Q)
+# plot.sim <- function(s=1,t=10,root=root,opaa=opaa,beta,gamma,bfaa=bfaa,func=TRUE,dist=TRUE, add=FALSE){
+#    dismat = GM_cpv(GM_CPV,al,beta,gamma)
+#    mumat = aa_MuMat_form(res_op$Q)
+#   sim <- simulation(root,opaa,t=t,s=s,DisMat=dismat,MuMat=mumat,bfaa=bfaa) #simulation
+#   l <- dim(sim)[2] - 2
+#   fty <- apply(sim[,1:l],MARGIN=1,FUN=Ftny_protein,protein_op=opaa,s=s,DisMat=dismat)#functionality
+#   dis <- apply(sim[,1:l],MARGIN=1,FUN=pchem_d,protein2=opaa,DisMat=dismat)#distance from optimal amino acids
+#   dis <- apply(dis,2,sum)#sum of distances for all sites
+#   ftyfun <- stepfun(sim[-1,l+1],fty,f=0,right=FALSE)#make step functions
+#   disfun <- stepfun(sim[-1,l+1],dis,f=0,right=FALSE)
+#   if(func) #plot functionality vs time
+#     plot(ftyfun,xlab="time",ylab="functionality",main=paste("functionality, s=",round(s,3),sep=""),pch=20,xlim=c(0,t),xaxs="i",add=add)
+#   if(dist) #plot distance vs time
+#     plot(disfun,xlab="time",ylab="distance",main=paste("distance, s=",round(s,3),sep=""),pch=20,xlim=c(0,t),xaxs="i",add=add)
+#   #return(as.numeric(tail(sim,1)[1:l])) #the sequence at the end of simulation
+#    return(list(sim=sim,fty=fty,dis=dis,ftyfun=ftyfun,disfun=disfun)) #store the simulation result for later use
+# }
+# sim <- plot.sim(s=s,t=br_max[genenum],root=root,opaa=opaa,beta,gamma,func=TRUE,dist=TRUE)
+# 
+# plot(sim83$ftyfun,xlab="time",ylab="functionality",main="Functionality",pch=20,xlim=c(0,br_max[83]),ylim=c(0.15,0.8),xaxs="i",col="blue")
+# plot(sim72$ftyfun,pch=20,xaxs="i",add=TRUE,col="red")
+# legend(5,y=0.7,legend=c("g=1.10, length=417","g=5.49, length=309"),col=c("blue","red"),lty=1,bty="n")
+# 
+# plot(sim83$disfun,xlab="time",ylab="distance",main="Distance from optima",pch=20,xlim=c(0,br_max[83]),ylim=c(0,400),xaxs="i",col="blue")
+# plot(sim72$disfun,pch=20,xaxs="i",add=TRUE,col="red")
+# legend(5,y=350,legend=c("g=1.10, length=417","g=5.49, length=309"),col=c("blue","red"),lty=1,bty="n")
+# 
+# plot.w <- function(x,add=FALSE){
+#   plot(wsim[[x]]$ftyfun,xlab="time",ylab="functionality",ylim=c(0.2,0.8),main=paste("functionality, s=",round(s,3),sep=""),do.points=FALSE,xlim=c(0,brlen),xaxs="i",add=add)
+#   plot(wsimWag[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
+#   plot(sim[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE)
+#   plot(simWag[[x]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
+# }
+# plot(wsim[[1]]$ftyfun,xlab="time",ylab="functionality",ylim=c(0,1),main=paste("functionality, s=",round(s,3),sep=""),do.points=FALSE,xlim=c(0,brlen),xaxs="i")
+# plot(wsimWag[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
+# plot(sim[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE)
+# plot(simWag[[1]]$ftyfun,xaxs="i",do.points=FALSE,xaxs="i",add=TRUE,col="red")
+# 
+# plot.func <- function(gene_num,count=3,margin=0.1){
+#   #filename <- paste("~/BackupProEvo/Lab9/prunetree/gene",gene_num,".RData",sep="")
+#   wfilename <- paste("~/BackupProEvo/Lab9/Wagprunetree/wgene",gene_num,".RData",sep="") #RData file with simulated data
+#   #load(filename)
+#   load(wfilename)
+#   obs.fty <- Ftny_protein(protein=datanum[6,],protein_op=opaa,s=s,DisMat=dismat)
+#   ## find the limits for y-axis
+#   fty_lim <- rbind(range(wsimWag[[1]]$fty),range(wsim[[1]]$fty),range(sim[[1]]$fty),range(simWag[[1]]$fty),rep(obs.fty,2)) ##ranges of functionalities for several typical simulations
+#   ylim <- c(min(fty_lim[,1]),max(fty_lim[,2])) #lower and upper bound
+#   ylim <- c(ylim[1]-ylim[1]*margin,ylim[2]+ylim[2]*margin) #extend them with a little margin
+#   
+#   plot(sim[[1]]$ftyfun,do.points=FALSE,xaxs="i",col="red",xlab="time",ylab="functionality",ylim=ylim,xlim=c(0,brlen),
+#        main=paste("gene",gene_num,",  s=",round(s_max[gene_num],2),sep="")) #start a plot
+#   abline(h=obs.fty,col="green",lwd=2)
+#   for(i in 1:count){
+#     plot(sim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
+#     plot(simWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
+#     plot(wsim[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="red",add=TRUE)
+#     plot(wsimWag[[i]]$ftyfun,do.points=FALSE,xaxs="i",col="blue",add=TRUE)
+#   }
+# }
--- pkg/R/test.R
+++ pkg/R/test.R
@@ -1,165 +1,45 @@
-nu_comb <- combinations(4,2,v=Nu) #combinations of nucleotides
-nu_rates <- apply(nu_comb,MARGIN=1,c2s) #convert numeric pairs into characters "ac","ag", etc
-# 
-###########################################################################
-## Matrix of dimension 61 by 61, each entry is the index of mutation rate 
-## between the 2 codons in the 6-element vector NU_rates
-diff_pos <- matrix(0,61,61)
-diag(diff_pos) <- 0
-for(i in 1:60){
-  for(j in (i+1):61){
-    cmp <- cdcmp(s2c(CDS[i]),s2c(CDS[j]))
-    if(cmp$num==1){
-      nu_from <- s2c(CDS[i])[cmp$pos]
-      nu_to <- s2c(CDS[j])[cmp$pos]
-      nu_pair <- c2s(sort(c(nu_from,nu_to)))
-      diff_pos[i,j] <- as.numeric(factor(nu_pair,levels=nu_rates,labels=1:6))
-    }
-    else
-      diff_pos[i,j] <- 0
-  }
-}
-diff_pos <- diff_pos + t(diff_pos)
-dimnames(diff_pos)[[1]] <- CDS #rownames and colnames are the amino acid names
-dimnames(diff_pos)[[2]] <- CDS
-###########################################################################
-## indAApair: a list of length 190, each corresponds to a pair of amino acids
-## including the number of possible ways of chaning to each other, and the rates for all the ways
-AApairs <- combinations(20,2)
-AApairs <- sapply(1:190,function(x) AApairs[x,],simplify=FALSE)
-indAApair <- vector("list",length(AApairs))
-for(i in 1:length(AApairs)){
-  diffmat <- diff_pos[cdlist[[AApairs[[i]][1]]],cdlist[[AApairs[[i]][2]]]]
-  if(sum(diffmat)!=0){
-    indAApair[[i]]$rates <- as.numeric(diffmat[diffmat!=0]) #index of rates in NU_rates
-    indAApair[[i]]$tot <- length(diffmat) #total number of codon comb for the pair of AA's
-  }
-  else{
-    indAApair[[i]]$rates <- 0
-    indAApair[[i]]$tot <- 1
-  }
-  indAApair[[i]]$AA <- AA[AApairs[[i]]]
-}
-
 ###########################################################################
 ## look at the nonzero entries in the AArate matrix, and try to figure out the 
 ## influence of NU_rates on AA_rates
-nonzeroPair <- indAApair[!sapply(indAApair,FUN=is.null)]
-sapply(1:length(nonzeroPair),function(x) nonzeroPair[[x]]$rates)
-sapply(1:length(nonzeroPair),function(x) nonzeroPair[[x]]$tot)
-NU_to_AA <- matrix(0,nrow=6,ncol=length(nonzeroPair))
-for(i in 1:length(nonzeroPair)){
-  rates <- nonzeroPair[[i]]$rates
-  NU_to_AA[,i] <- sapply(1:6,function(x) sum(rates==x))/nonzeroPair[[i]]$tot
-}
-
-
+# nonzeroPair <- indAApair[!sapply(indAApair,FUN=is.null)]
+# sapply(1:length(nonzeroPair),function(x) nonzeroPair[[x]]$rates)
+# sapply(1:length(nonzeroPair),function(x) nonzeroPair[[x]]$tot)
+# NU_to_AA <- matrix(0,nrow=6,ncol=length(nonzeroPair))
+# for(i in 1:length(nonzeroPair)){
+#   rates <- nonzeroPair[[i]]$rates
+#   NU_to_AA[,i] <- sapply(1:6,function(x) sum(rates==x))/nonzeroPair[[i]]$tot
+# }
 # get.root <- function(mat){
 #   l <- dim(mat)[2]
 #   sapply(1:l,function(x) which(mat[,x]==1))
 # }
+optimQ <- function(tree,data,Q=rep(1,6),method="SBPLX",maxeval="100",print_level=0, ...){
+  Q = Q/Q[6] #make last rate 1
+  #store information from initial condition, with other parameters fixed
+  res.initial = mllm1(data=data,tree=tree,Q=Q,...)
+  #these don't change with the change of s
+  fixmatall = res.initial$fixmatall
+  
+  ab <- Q[1:5] # optimize on the first 5 rates
+  fn = function(ab,tree,data){
+    cat(ab,"\n")
+    result = -mllm1(data,tree,Q=c(ab,1),fixmatall=fixmatall, ...)$ll$loglik
+    return(result)
+  }
+  lower=rep(0,5)
+  upper=rep(Inf,5)
+  #options for optimizer
+  opts <- list("algorithm"=paste("NLOPT_LN_",method,sep=""),"maxeval"= maxeval,"xtol_rel"=1e-6,
+               "ftol_rel"=.Machine$double.eps^0.5,"print_level"=print_level)
+  res = nloptr(x0=ab,eval_f=fn, lb=lower,ub=upper,opts=opts,data=data,tree=tree)
+  res$solution = c(res$solution,1) # append the last rate (1) to the rate vector
+  return(res)
+}
 
-
-# simulationQ1 <- function(protein,t,Q=NULL,bf=NULL){
-#   if(is.null(bf)) bf <- rep(1/20,20)
-#   if(is.null(Q)) Q <- rep(1,190)
-#   if(is.matrix(Q)) Qmat <- Q
-#   else if(is.vector(Q))  
-#     Qmat <- mat_form_lowtriQ(Q,bf,byrow=TRUE) #transition rate matrix
-#   l <- length(protein) #number of sites
-#   P <- expm(Qmat*t)
-#   res <- protein
-#   for(i in 1:20){
-#     index <- which(protein==i)
-#     res[index] <- sample(1:20,length(index),replace=TRUE,prob=P[i,])
-#   }
-#   return(res)
-# }
-# 
-# find.Q <- function(seq1,seq2,Q=rep(1,6),t=1,method="COBYLA",maxeval="1000",print_level=0){
-#   fn <- function(Q){
-#     Q <- exp(Q)
-#     mumat <- aa_MuMat_form(Q)
-#     P <- expm(mumat*t)
-#     prob <- -log(sapply(1:length(seq1), function(i) P[seq1[i],seq2[i]]))
-#     cat(Q,sum(prob),"\n")
-#     return(sum(prob))
-#   }
-#   lower=rep(-Inf,6)
-#   upper=rep(Inf,6)
-#   #options for optimizer
-#   opts <- list("algorithm"=paste("NLOPT_LN_",method,sep=""),"maxeval"= maxeval,"xtol_rel"=1e-6,
-#                "ftol_rel"=.Machine$double.eps^0.5,"print_level"=print_level)
-#   res = nloptr(x0=log(Q),eval_f=fn, lb=lower,ub=upper,opts=opts)
-#   #optimx(log(Q),fn,hessian=FALSE,method=method,itnmax=itnmax)
-#   return(res)
-# }
-# 
-# find.Q1 <- function(seq1,seq2,q=1,t=1,interval=c(-10,10)){
-#   fn <- function(q){
-#     q = exp(q)
-#     Q <- rep(q,6)
-#     mumat <- aa_MuMat_form(Q)
-#     P <- expm(mumat*t)
-#     prob <- -log(sapply(1:length(seq1), function(i) P[seq1[i],seq2[i]]))
-#     cat(q,sum(prob),"\n")
-#     return(sum(prob))
-#   }
-#   optimize(f=fn,interval=interval)
-# }
-# 
-# optim.ultrametric <- function(tree,data,method="SBPLX",maxeval="100",print_level=1,...){
-#   ## free parameters: total tree depth, all the internal branch lengths
-#   ## calculate the exterior branch lengths given the free parameters
-#   nTips <- length(tree$tip.label)
-#   nIntBr <- nTips - 2 #number of interior branches
-#   edge <- tree$edge
-#   int.ind <- which(!(edge[,2] %in% c(1:nTips))) #index of interior branches in tree$edge
-#   ext.ind <- which(edge[,2] %in% c(1:nTips))
-#   fn <- function(x){ ## x <- c(intBrlen, depth)
-#     x <- exp(x)
-#     cat(x,"\n")
-#     depth <- x[nIntBr + 1] # tree depth is the last element in the vector
-#     tree$edge.length[int.ind] <- x[1:nIntBr] # assign internal branches new lengths
-#     tree <- ult.tree(tree,depth=depth)$tree #calculate exterior branch lengths
-#     cat("tree edge lengths","\n")
-#     cat(tree$edge.length,"\n")
-#     if(any(tree$edge.length<0)) return(10^7)
-#     else{
-#       res <- -mllm1(data,tree,...)$ll$loglik
-#       cat("loglik = ", res, "\n")
-#       return(res)
-#     }
-#   }
-#   ## constraint function: add "eval_g_ineq=g0" in the nloptr function
-# #   g0 <- function(x){
-# #     x <- exp(x)
-# #     depth <- x[nIntBr + 1]
-# #     tree$edge.length[int.ind] <- x[1:nIntBr] # assign internal branches new lengths
-# #     tree <- ult.tree(tree,depth=depth) #calculate exterior branch lengths
-# #     return(-tree$edge.length[ext.ind])
-# #   }
-#   lower=rep(-Inf,nIntBr+1)
-#   upper=rep(Inf,nIntBr+1)
-#   #options for optimizer
-#   opts <- list("algorithm"=paste("NLOPT_LN_",method,sep=""),"maxeval"= maxeval,"xtol_rel"=1e-6,
-#                "ftol_rel"=.Machine$double.eps^0.5,"print_level"=print_level)
-#   
-#   res = nloptr(x0=log(c(tree$edge.length[int.ind],max(node.depth.edgelength(tree)))),
-#                eval_f=fn, lb=lower,ub=upper,opts=opts)
-#   x <- res$solution
-#   x <- exp(x)
-#   depth <- x[nIntBr + 1] # tree depth is the last element in the vector
-#   tree$edge.length[int.ind] <- x[1:nIntBr] # assign internal branches new lengths
-#   tree <- ult.tree(tree,depth=depth)$tree #calculate exterior branch lengths
-#   return(list(res=res,tree=tree))
-# }
-
-# internal.br <- function(tree){
-#   nTips <- length(tree$tip.label)
-#   edge <- tree$edge
-#   internal.ind <- which(!(edge[,2] %in% c(1:nTips)))
-#   return(list(index = internal.ind,edge=edge[internal.ind,],edge.length=tree$edge.length[internal.ind]))
-# }
-
-
+hessianQ <- function(mle,data,tree,...){
+  fn <- function(Q){
+    print(Q)
+    return(-mllm1(data=data,tree=tree,Q=Q,...)$ll$loglik)
+  }
+  return(hessian(fn,mle,method="Richardson"))
+}
